1ac1eca405dce3c48190eb336e205bcd82c1228b
Jason L. van Brackel <jason@vanbrackel.net>
2017-06-16 15:06:56 -0400

    Initial code commit.

--- SqlServerConnectionAnalyzer/Properties/AssemblyInfo.cs
+++ SqlServerConnectionAnalyzer/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SqlServerConnectionAnalyzer")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Tool for analyzing SQL Server Connectivity Problems")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("SqlServerConnectionAnalyzer")]
-[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyCompany("Apprenda Client Services")]
+[assembly: AssemblyProduct("SqlServer Connection Analyzer")]
+[assembly: AssemblyCopyright("Apprenda Client Services ©  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.0.0")]
+[assembly: AssemblyFileVersion("0.0.0.0")]
